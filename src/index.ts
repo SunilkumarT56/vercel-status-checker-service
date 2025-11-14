767f1876723dfb3aa8b1ffe992e05ca6a8e83869
Sunilkumar <sunilcse2006@gmail.com>
2025-11-14 22:48:15 +0530

    change route

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import express from "express"
 import redis from "redis"
 import dotenv from "dotenv"
-dotenv.config()
-
+dotenv.config();
 const PORT = process.env.PORT
 
 const subscriber = redis.createClient();
@@ -14,7 +13,7 @@ subscriber.on("ready", () => console.log("Redis connected"));
 
 const app = express()
 
-app.get("/status" , async (req, res) => {
+app.get("/api/status" , async (req, res) => {
     const {id} = req.query
     if (typeof id !== 'string') {
         return res.status(400).json({ error: 'Invalid id parameter' });
