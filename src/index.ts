93a6e0363dc35588e4cc4f58c6905b7e31dcd7f2
Sunilkumar <sunilcse2006@gmail.com>
2025-11-16 15:03:34 +0530

    d

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,24 @@
-import express from "express"
-import redis from "redis"
-import dotenv from "dotenv"
+import express from "express";
+import redis from "redis";
+import dotenv from "dotenv";
 dotenv.config();
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-const subscriber = redis.createClient(
-    {
-  socket: {
-    host: process.env.REDIS_HOST || "redis",
-    port: Number(process.env.REDIS_PORT) || 6379,
-  }
-}
-
-);
+const subscriber = redis.createClient();
 subscriber.connect();
 subscriber.on("ready", () => console.log("Redis connected"));
 
+const app = express();
 
+app.get("/api/status", async (req, res) => {
+  const { id } = req.query;
+  if (typeof id !== "string") {
+    return res.status(400).json({ error: "Invalid id parameter" });
+  }
+  const response = await subscriber.hGet("status", id);
+  res.json({ status: response });
+});
 
-
-const app = express()
-
-app.get("/api/status" , async (req, res) => {
-    const {id} = req.query
-    if (typeof id !== 'string') {
-        return res.status(400).json({ error: 'Invalid id parameter' });
-    }
-    const response = await subscriber.hGet("status", id);
-    res.json({ status: response });
-
-
-
-
-
-})
-
-app.listen(PORT , () => {
-    console.log("Server is running on port 5001")
-
-})
+app.listen(PORT, () => {
+  console.log("Server is running on port 5001");
+});
